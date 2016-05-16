8a2bad3f3b235647c2cf6737ab0b03cbbdb82400
Megas <Megas@194.210.241.0>
2016-05-16 16:23:34 +0100

    Jogo já com movimento

--- src/Jogo/Esfera.java
+++ src/Jogo/Esfera.java
@@ -6,48 +6,61 @@
 package Jogo;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 
 /**
  *
  * @author Megas
  */
-public class Esfera extends Forma{
-    private Ellipse2D.Double circulo;
-    
-    public Esfera(double x, double y, double raio, Color cor)
+public class Esfera implements Mover{
+    private final Ellipse2D.Double circulo;
+    private final Point2D.Double ponto;
+    private final Color cor;
+
+    public Esfera(double x, double y, double raio, Color c)
     {
-        super(x, y, raio, cor);
         circulo = new Ellipse2D.Double(x, y, raio, raio);
+        ponto = new Point2D.Double(x, y);
+        cor = c;
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-        //Renderiza as bordas do circulo(nao pixelizado)
-        g2d.setRenderingHint(
-        RenderingHints.KEY_ANTIALIASING,
-        RenderingHints.VALUE_ANTIALIAS_ON);//anti-aliasing
-        g2d.setColor(cor);
-        g2d.fill(circulo);
+    public Color getCor() {
+        return cor;
     }
-    
-    public void setEllipseX(int x)
-    {
-        circulo.x = x;
-    }
-    
-    public void setEllipseY(int y)
-    {
-        circulo.y = y;
+
+    public Point2D.Double getPonto() {
+        return ponto;
     }
     
     @Override
-    public Shape getForma()
+    public void incrementaX()
+    {
+        circulo.x = circulo.x + 0.5;
+        ponto.setLocation(circulo.x, circulo.y);
+    }
+    
+    @Override
+    public void decrementaX()
+    {
+        circulo.x = circulo.x - 0.5;
+        ponto.setLocation(circulo.x, circulo.y);
+    }
+    @Override
+    public void incrementaY()
+    {
+        circulo.y = circulo.y + 0.5;
+        ponto.setLocation(circulo.x, circulo.y);
+    }
+    @Override
+    public void decrementaY()
+    {
+        circulo.y = circulo.y - 0.5;
+        ponto.setLocation(circulo.x, circulo.y);
+    }
+    
+    public Shape getCirculo()
     {
         return circulo;
     }
