f13684ce96ce703ab7b441e2cb4631f392866d76
Megas <Megas@194.210.241.0>
2016-05-16 16:57:16 +0100

    ultima versão

--- src/Jogo/Painel.java
+++ src/Jogo/Painel.java
@@ -8,24 +8,32 @@ package Jogo;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.MouseInfo;
+import java.awt.geom.Point2D;
 import java.awt.RenderingHints;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 /**
  *
  * @author Megas
  */
-public class Painel extends JPanel{
-    private final ArrayList<Forma> lista;
-    private final int largura = 11000;
-    private final int altura = 11000;
+public class Painel extends JPanel implements Runnable{
+    private Thread t;
+    private final ArrayList<Esfera> lista;
+    private final int tamanho = 11000;
     private int tamanhoInicialBolas;
     private Esfera jogador;
+    private Point2D localizacao;
+    private final int velocidade = 3;
     
     public Painel()
     {
         super();
+        t = new Thread(this);
         this.setBackground(Color.white);
         lista = new ArrayList();
     }
@@ -35,13 +43,89 @@ public class Painel extends JPanel{
         tamanhoInicialBolas = (this.getWidth()+this.getHeight())/6;
         for(int i = 0; i < 7000;i++)
             this.inserir();
-        inserirJogador(tamanhoInicialBolas);
+        inserirJogador();
     }
     
-    public final void inserirJogador(int raio)
+    public final void inserirJogador()
     {
-       jogador = new Esfera(this.getWidth(), this.getHeight(), raio, new Color((float)Math.random(), (float)Math.random(), (float)Math.random()));
+       Point2D p = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
+       jogador = new Esfera(p.getX(), p.getY(), 100, new Color((float)Math.random(), (float)Math.random(), (float)Math.random()));
+       localizacao = p;
     }
+    
+    public void jogar()
+    {
+        t.start();
+    }
+    
+    public void moverJogador()
+    {
+       Point2D pontoJogador = jogador.getPonto();
+       Point2D rato = MouseInfo.getPointerInfo().getLocation();
+       int t = lista.size();
+       Esfera temp;
+       //if(localizacao.getX() <= 0 || localizacao.getX() >= tamanho)
+           //return;
+      // if(localizacao.getY() <= 0 || localizacao.getY() >= tamanho)
+           //return;
+           
+        if (pontoJogador.getX() > rato.getX())
+       {
+           for(int i = 0; i < t;i++)
+           {
+                temp = lista.get(i);
+                temp.incrementaX(velocidade);
+                lista.set(i, temp);
+                localizacao.setLocation(localizacao.getX()+velocidade, localizacao.getY());
+           }
+       }
+       else
+       {
+           for(int i = 0; i < t;i++)
+           {
+                temp = lista.get(i);
+                temp.decrementaX(velocidade);
+                lista.set(i, temp);
+                localizacao.setLocation(localizacao.getX()-velocidade, localizacao.getY());
+           }
+       }
+       
+       if (pontoJogador.getY() > rato.getY())
+       {
+           for(int i = 0; i < t;i++)
+           {
+                temp = lista.get(i);
+                temp.incrementaY(velocidade);
+                lista.set(i, temp);
+                localizacao.setLocation(localizacao.getX(), localizacao.getY()+velocidade);
+           }
+       }
+       else
+       {
+           for(int i = 0; i < t;i++)
+           {
+                temp = lista.get(i);
+                temp.decrementaY(velocidade);
+                lista.set(i, temp);
+                localizacao.setLocation(localizacao.getX(), localizacao.getY()-velocidade);
+           }
+       }
+    }
+       
+    @Override
+       public void run()
+       {
+           while(true)
+           {
+               moverJogador();
+               this.repaint();
+               try {
+                   Thread.sleep(1);
+               } catch (InterruptedException ex) {
+                   Logger.getLogger(Painel.class.getName()).log(Level.SEVERE, null, ex);
+               }
+           }
+       }
     
     public final void inserir()
     {
@@ -50,7 +134,7 @@ public class Painel extends JPanel{
         float b = (float)Math.random();
         if(r <= 0.5 && g <= 0.5 && b <= 0.5)
             r = (float) 0.75;
-        lista.add(new Esfera(Math.random()*(largura), Math.random()*(altura), tamanhoInicialBolas, new Color(r,g,b)));
+        lista.add(new Esfera(Math.random()*(tamanho), Math.random()*(tamanho), tamanhoInicialBolas, new Color(r,g,b)));
     }
     
     @Override
@@ -58,15 +142,16 @@ public class Painel extends JPanel{
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
         //Renderiza as bordas do circulo(nao pixelizado)
-        g2d.setRenderingHint(
+        /*g2d.setRenderingHint(
         RenderingHints.KEY_ANTIALIASING,
-        RenderingHints.VALUE_ANTIALIAS_ON);//anti-aliasing
+        RenderingHints.VALUE_ANTIALIAS_ON);//anti-aliasing*/
         lista.stream().map((a) -> {
             g2d.setColor(a.getCor());
             return a;
         }).forEach((a) -> {
-            g2d.fill(a.getForma());
+            g2d.fill(a.getCirculo());
         });
-        g2d.fill(jogador.getForma());
+        g2d.fill(jogador.getCirculo());
+        repaint();
     }
 }
